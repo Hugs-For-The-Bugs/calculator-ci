8a30db1952f13ce3479ffd38ad3b90b8688be19d
Lambo Zhuang <Lambozhuang@gmail.com>
2024-02-27 15:20:37 +0100

    test added: addTest

--- src/test/java/com/hugsforthebugs/CalculatorTest.java
+++ src/test/java/com/hugsforthebugs/CalculatorTest.java
@@ -5,5 +5,10 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+    @Test
+    public void addTest()
+    {
+        Calculator calculator = new Calculator();
+        assertTrue( calculator.add(1, 1) == 2 );
+    }
 }
