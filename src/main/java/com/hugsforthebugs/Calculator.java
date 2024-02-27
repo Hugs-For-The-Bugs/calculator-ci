30a6a4a399f369fabcd34ba6810774966b877cc7
manamin0521 <17105761+manamin0521@users.noreply.github.com>
2024-02-27 05:14:15 +0100

    feat: divide

--- src/main/java/com/hugsforthebugs/Calculator.java
+++ src/main/java/com/hugsforthebugs/Calculator.java
@@ -10,6 +10,9 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
         return a / b;
     }
 }
