65b5887d6cef0828ad24cd9090065177563772a9
manamin0521 <17105761+manamin0521@users.noreply.github.com>
2024-03-09 13:15:06 +0100

    Add isPrime method

--- src/main/java/com/hugsforthebugs/Calculator.java
+++ src/main/java/com/hugsforthebugs/Calculator.java
@@ -90,4 +90,16 @@ public class Calculator {
         }
         return a % b;
     }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+          return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i == 0) {
+            return false;
+          }
+        }
+        return true;
+      }
 }
